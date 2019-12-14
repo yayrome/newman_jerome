1d952bf6e79c889a41f49d663168f97646893a12
Jerome Newman <JeromeNewman.cs@gmail.com>
2019-12-13 19:31:03 -0600

    WIP: React front end setup

---
+++ newman_jerome_fe/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className = "app" >
+                <h1>Hi There</h1>
+            </div>
+        );
+    }
+}
