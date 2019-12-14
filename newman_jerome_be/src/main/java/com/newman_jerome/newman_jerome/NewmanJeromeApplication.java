d9cb9736e537542299624ee90fe54e193d493fab
Jerome Newman <JeromeNewman.cs@gmail.com>
2019-12-13 18:35:29 -0600

    Initial Commit

---
+++ newman_jerome_be/src/main/java/com/newman_jerome/newman_jerome/NewmanJeromeApplication.java
@@ -0,0 +1,13 @@
+package com.newman_jerome.newman_jerome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmanJeromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewmanJeromeApplication.class, args);
+	}
+
+}
